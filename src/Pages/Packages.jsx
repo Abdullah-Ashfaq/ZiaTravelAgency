7c61df8e25adb3ef4c5a0af782c8716d2cfa2325
Abdullah <mughalian911@gmail.com>
2023-03-02 03:04:18 +0500

    updated

--- src/Pages/Packages.jsx
+++ src/Pages/Packages.jsx
@@ -1,14 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
 import Navbar from '../component/Navbar'
 import Services from "../component/Services"
 import Footer from "../component/Footer"
 import "../css/Packages.css"
 import { MenuItem, TextField } from '@mui/material'
+import axios from 'axios'
+
+
+
 
 
 const Packages = () => {
   const[filterPackage, setFilterPacakge] = useState('All')
+  const [data, setData] = useState([])
  
+  
+ 
+     const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://63f157ed5703e063fa560d2c.mockapi.io/packages`);
+        setData(response.data)
+        console.log(data)
+        
+      } catch (error) {
+        console.log("Error while fetching the packages from an api")
+      }
+      
+    }
+ 
+  
+useEffect(() => {
+  fetchData(); 
+}, [])
   return (
     <>
     <Navbar></Navbar>
@@ -16,7 +39,9 @@ const Packages = () => {
 
       <div style={{marginTop:"1px"}}></div>
 
+    {/* calling the Service component with the prop */}
       <Services name = "packages"/>
+
 
       <div className="packages">
 
@@ -32,12 +57,9 @@ const Packages = () => {
           sx={{
             width:'220px',
             color:'green'
-            
         }}
-          onChange={(event) => setFilterPacakge(event.target.value) }
-          
-          
-          >
+          onChange={(event) => setFilterPacakge(event.target.value) }>
+            
             <MenuItem value = 'Hajjandumrah'>Hajj & Umrah</MenuItem>
             <MenuItem value = 'dubai'>Dubai</MenuItem>
             <MenuItem value = 'iran'>Iran</MenuItem>
@@ -50,9 +72,31 @@ const Packages = () => {
           
         </div>
         <div className="packages-container">
-          <div className="package"> </div>
-          <div className="package"> </div>
-          <div className="package"> </div>
+          {
+            data.map((val) => {
+              return (
+                <div className="package" key={val.id}>
+                <div className="package-top">
+                  <div className="text"><span>Category:</span> {` ${val.category}`}</div>
+                  <div className="text"><span>Country:</span>{` ${val.country}`}</div>
+                  <div className="text"><span>Duration:</span>{` ${val.duration}`}</div>
+                  <div className="text"><span>Price:</span>{` ${val.price}`}</div>
+                </div>
+    
+                <div className="text"><span>About:</span>{`  ${val.about}`}</div>
+                <img className='package-img' src={val.file} alt="" />
+    
+              </div>
+
+              )
+              
+             
+            
+            
+            })
+          }
+        
+          
         </div>
 
       </div>
