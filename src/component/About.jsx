400ff87af305e553d7643e67e0486eb198ddb1db
Abdullah <mughalian911@gmail.com>
2023-03-30 03:11:17 +0500

    updated

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -13,10 +13,13 @@ const About = () => {
 
       <div className="right-side-cont">
 
-        <span>"</span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum enim, voluptas ea ad,
-        magni pariatur sed explicabo fuga harum laboriosam minus soluta. Vero nam eum ut non nihil explicabo praesentium?
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam fugit aliquid officia facere quia distinctio saepe ullam autem laudantium, eos recusandae assumenda
-        aperiam fuga alias tempore dignissimos consequuntur deserunt ex.<span>"</span>
+        <span>"</span>Over the past 15 years, our travel agency based in the heart of Islamabad has been providing a range of travel services to our clients. We specialize in offering Hajj and Umrah services, visit visas for Dubai, Thailand, Malaysia, and Indonesia, as well as Zayarat services for Iran and Iraq.
+
+Our team of experienced professionals is dedicated to ensuring that our clients have a hassle-free travel experience, from booking their flights and accommodations to arranging for transportation and tours. We pride ourselves on providing excellent customer service and personalized attention to each of our clients.
+
+With our years of expertise and knowledge in the travel industry, we have established a reputation for reliability and trustworthiness. Our goal is to make sure that our clients have a safe, comfortable, and memorable journey that they will cherish for a lifetime.
+
+We are committed to continuing to provide quality travel services to our clients, and we look forward to serving their needs for many years to come.<span>"</span>
 
 
 
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -41,6 +41,9 @@ const Footer = () => {
 
         </div>
       </footer>
+      <div className="copyright">
+        <p>Copyright &copy; 2023 Zia Travel and Tours Pvt Ltd. All rights reserved.</p>
+      </div>
     </>
   )
 }
