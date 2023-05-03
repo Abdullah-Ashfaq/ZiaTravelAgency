dd7d23b564ede96c6661b254b0032998df6a6765
Abdullah <mughalian911@gmail.com>
2023-05-03 06:17:51 +0500

    updated

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -15,11 +15,11 @@ const About = () => {
 
         <span>"</span>Over the past 15 years, our travel agency based in the heart of Islamabad has been providing a range of travel services to our clients. We specialize in offering Hajj and Umrah services, visit visas for Dubai, Thailand, Malaysia, and Indonesia, as well as Zayarat services for Iran and Iraq.
 
-Our team of experienced professionals is dedicated to ensuring that our clients have a hassle-free travel experience, from booking their flights and accommodations to arranging for transportation and tours. We pride ourselves on providing excellent customer service and personalized attention to each of our clients.
+        Our team of experienced professionals is dedicated to ensuring that our clients have a hassle-free travel experience, from booking their flights and accommodations to arranging for transportation and tours. We pride ourselves on providing excellent customer service and personalized attention to each of our clients.
 
-With our years of expertise and knowledge in the travel industry, we have established a reputation for reliability and trustworthiness. Our goal is to make sure that our clients have a safe, comfortable, and memorable journey that they will cherish for a lifetime.
+        With our years of expertise and knowledge in the travel industry, we have established a reputation for reliability and trustworthiness. Our goal is to make sure that our clients have a safe, comfortable, and memorable journey that they will cherish for a lifetime.
 
-We are committed to continuing to provide quality travel services to our clients, and we look forward to serving their needs for many years to come.<span>"</span>
+        We are committed to continuing to provide quality travel services to our clients, and we look forward to serving their needs for many years to come.<span>"</span>
 
 
 
